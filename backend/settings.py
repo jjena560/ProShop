b638367e3c87b05278c3727c9eb8e0626f966482
jjena560 <jjena560@gmail.com>
2021-05-16 23:03:16 +0530

    directory_change

--- backend/settings.py
+++ backend/settings.py
@@ -172,18 +172,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' 
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,  'static'),   
-    # os.path.join(BASE_DIR, 'frontend/build/static')
-     os.path.join(os.path.join(BASE_DIR, 'frontend'), 'build', 'static'),
+    # os.path.join(BASE_DIR,  'static'),   
+    os.path.join(BASE_DIR, 'frontend/build/static'),
+    # os.path.join(os.path.join(BASE_DIR, 'frontend'), 'build', 'static'),
 
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
